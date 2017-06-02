2bc90f64db99d77a1ea897e0fa651a7a61b58210
Peter DeMartini <thepeterdemartini@gmail.com>
2017-06-02 11:35:22 -0700

    v3.0.0 use latest meshblu-config

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-require('coffee-script/register')
-require('fs-cson/register')
-module.exports=require('./src/schema-generator.coffee')
+var pathExists = require('path-exists')
+var theFile
+
+if (pathExists.sync('./dist/index.js')) {
+  theFile = require('./dist')
+} else {
+  require('coffee-script/register');
+  require('fs-cson/register')
+  theFile = require('./src')
+}
+
+module.exports = theFile;
